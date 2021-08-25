b37465d157dc11d0e3449c64471ae6570f7abb1c
Hadi <hadinasser2.01@gmail.com>
2021-08-25 11:07:26 +0300

    first try

---
+++ 142testGIT.py
@@ -0,0 +1,25 @@
+from flask import Flask, request
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=["POST", "GET"])
+def webhook():
+    if request.method == "GET":
+        return "Not connected to DF"
+    elif request.method == "POST":
+        payload = request.json
+        user_response = (payload['queryResult']['queryText'])
+        bot_response = (payload['queryResult']['fulfillmentText'])
+        if user_response or bot_response != "":
+            print("user: " + user_response)
+            print("bot: " + bot_response)
+        return "msg received"
+    else:
+        print("request.data")
+        return "200"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
