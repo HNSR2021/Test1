d950c6c91250e04ce1a8d40053190a37b075333d
Hadi <hadinasser2.01@gmail.com>
2021-08-25 11:50:59 +0300

    changetest

--- 142testGIT.py
+++ 142testGIT.py
@@ -2,23 +2,27 @@ from flask import Flask, request
 
 
 app = Flask(__name__)
+@app.route('/')
+def hello():
+  return 'hello world'
 
 
-@app.route('/', methods=["POST", "GET"])
-def webhook():
-    if request.method == "GET":
-        return "Not connected to DF"
-    elif request.method == "POST":
-        payload = request.json
-        user_response = (payload['queryResult']['queryText'])
-        bot_response = (payload['queryResult']['fulfillmentText'])
-        if user_response or bot_response != "":
-            print("user: " + user_response)
-            print("bot: " + bot_response)
-        return "msg received"
-    else:
-        print("request.data")
-        return "200"
+
+# @app.route('/', methods=["POST", "GET"])
+# def webhook():
+   # if request.method == "GET":
+      #  return "Not connected to DF"
+   # elif request.method == "POST":
+     #   payload = request.json
+     #   user_response = (payload['queryResult']['queryText'])
+     #   bot_response = (payload['queryResult']['fulfillmentText'])
+      #  if user_response or bot_response != "":
+         #   print("user: " + user_response)
+        #    print("bot: " + bot_response)
+      #  return "msg received"
+   # else:
+      #  print("request.data")
+      #  return "200"
 
 
 if __name__ == '__main__':
